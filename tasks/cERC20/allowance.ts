30254cbd9624381ffcd8871766604d68fd61f6f8
cozfuttu <canozfuttu07@gmail.com>
2025-02-09 11:45:10 +0300

    feat: allowance task added

---
+++ tasks/cERC20/allowance.ts
@@ -0,0 +1,45 @@
+import { task } from "hardhat/config";
+
+task("allowance", "Get user allowance")
+  .addParam("signeraddress", "signer address")
+  .addParam("tokenaddress", "cERC20 contract address")
+  .addParam("spenderaddress", "Spender Address")
+  .addOptionalParam("address", "User Address")
+  .setAction(async function (
+    { signeraddress, tokenaddress, address, spenderaddress },
+    hre,
+  ) {
+    const { ethers, fhenixjs } = hre;
+    const signer = await ethers.getSigner(signeraddress);
+
+    if (!address) {
+      address = signer.address;
+    }
+
+    let contract = await ethers.getContractAt("cUSDC", tokenaddress, signer);
+
+    console.log(
+      `Running encryptedAllowance, targeting contract at: ${tokenaddress}`,
+    );
+
+    let permit = await fhenixjs.generatePermit(
+      tokenaddress,
+      undefined, // use the internal provider
+      signer,
+    );
+
+    console.log(`permit: ${JSON.stringify(permit)}`);
+
+    const permission = fhenixjs.extractPermitPermission(permit);
+
+    const encryptedResult = await contract.encryptedAllowance(
+      permission,
+      spenderaddress,
+    );
+    console.log(`got result: ${encryptedResult.toString()}`);
+
+    const sealedResult = await contract.getPermitSealed(permit);
+    let unsealed = fhenixjs.unseal(tokenaddress, sealedResult, signer.address);
+
+    console.log(`got unsealed result: ${unsealed.toString()}`);
+  });
--- tasks/index.ts
+++ tasks/index.ts
@@ -3,3 +3,4 @@ export * from "./cERC20/balanceOf";
 export * from "./cERC20/wrap";
 export * from "./cERC20/approve";
 export * from "./cERC20/transferFrom";
+export * from "./cERC20/allowance";
